e63aa7f3b25b99b4ad75c50d95b0efc355fe741d
대하 <daeha76@daum.net>
2023-03-02 07:49:27 +0900

    Learn Lifecycle Methods

--- Maui_Sample_Contacts/App.xaml.cs
+++ Maui_Sample_Contacts/App.xaml.cs
@@ -1,11 +1,39 @@
-﻿namespace Maui_Sample_Contacts;
+﻿using System.Diagnostics;
+
+namespace Maui_Sample_Contacts;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new MainPage();
+    }
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+        window.Created += (s, e) => { Debug.WriteLine("******** Created ********"); };
+        window.Activated += (s, e) => { Debug.WriteLine("******** Activated ********"); };
+        window.Deactivated += (s, e) => { Debug.WriteLine("******** Deactivated ********"); };
+        window.Stopped += (s, e) => { Debug.WriteLine("******** Stopped ********"); };
+        window.Resumed += (s, e) => { Debug.WriteLine("******** Resumed ********"); };
+        window.Destroying += (s, e) => { Debug.WriteLine("******** Destroying ********"); };
+        return window;
+    }
+
+    protected override void OnStart()
+    {
+        Debug.WriteLine("******** OnStart ********");
+    }
+
+    protected override void OnSleep()
+    {
+        Debug.WriteLine("******** OnSleep ********");
+    }
+
+    protected override void OnResume()
+    {
+        Debug.WriteLine("******** OnResume ********");
+    }
 }
